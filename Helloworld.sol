903872b525163a1929c96fecd0bdf14208e6fb6d
Remix Workspace <>
2022-07-31 22:31:50 +0530

    Compile helloworld.sol

--- Helloworld.sol
+++ Helloworld.sol
@@ -5,3 +5,21 @@ pragma solidity 0.4.20;
 contract Helloworld {
     string public greet = "Hello world";
 }
+
+
+[
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "greet",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
