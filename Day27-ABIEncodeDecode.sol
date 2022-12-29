2aa8b6b19eb1cd0dbe5d9035d731469cbf965537
Shiva Kumar Mangina <shiva2nani.mangina@gmail.com>
2022-12-29 21:15:19 +0530

    feat: Add All Smart contract

---
+++ Day27-ABIEncodeDecode.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+interface IERC20 {
+    function transfer(address, uint) external;
+}
+
+contract Token {
+    function transfer(address, uint) external {}
+}
+
+contract AbiEncode {
+    function test(address _contract, bytes calldata data) external {
+        (bool ok, ) = _contract.call(data);
+        require(ok, "call failed");
+    }
+
+    function encodeWithSignature(
+        address to,
+        uint amount
+    ) external pure returns (bytes memory) {
+        // Typo is not checked - "transfer(address, uint)"
+        return abi.encodeWithSignature("transfer(address,uint256)", to, amount);
+    }
+
+    function encodeWithSelector(
+        address to,
+        uint amount
+    ) external pure returns (bytes memory) {
+        // Type is not checked - (IERC20.transfer.selector, true, amount)
+        return abi.encodeWithSelector(IERC20.transfer.selector, to, amount);
+    }
+
+    function encodeCall(address to, uint amount) external pure returns (bytes memory) {
+        // Typo and type errors will not compile
+        return abi.encodeCall(IERC20.transfer, (to, amount));
+    }
+}
+
+
+contract AbiDecode {
+    struct MyStruct {
+        string name;
+        uint[2] nums;
+    }
+
+    function encode(
+        uint x,
+        address addr,
+        uint[] calldata arr,
+        MyStruct calldata myStruct
+    ) external pure returns (bytes memory) {
+        return abi.encode(x, addr, arr, myStruct);
+    }
+
+    function decode(
+        bytes calldata data
+    )
+        external
+        pure
+        returns (uint x, address addr, uint[] memory arr, MyStruct memory myStruct)
+    {
+        // (uint x, address addr, uint[] memory arr, MyStruct myStruct) = ...
+        (x, addr, arr, myStruct) = abi.decode(data, (uint, address, uint[], MyStruct));
+    }
+}
---
+++ Day29-MicropaymentChannel.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.10;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./SignUtils.sol";
+
+contract Micropayments is Ownable {
+    using SignUtils for bytes32;
+    using SignUtils for bytes;
+    mapping(uint256 => bool) private usedNonces;
+    string public name;
+    address public factory;
+
+    event PaymentClaimed(address indexed from, string name, uint256 at);
+
+    constructor(string memory _name) payable {
+        name = _name;
+        factory = _msgSender();
+    }
+
+    modifier onlyFromFactory() {
+        require(factory == _msgSender(), "Caller is not the creator");
+        _;
+    }
+
+    function claimPayment(
+        uint256 amount,
+        uint256 nonce,
+        bytes memory signature
+    ) external {
+        require(!usedNonces[nonce], "Claim already done");
+        usedNonces[nonce] = true;
+        bytes32 message = keccak256(abi.encodePacked(_msgSender(), amount, nonce, this)).prefixed();
+        require(message.recoverSigner(signature) == owner(), "Signature is invalid [owner]");
+        payable(_msgSender()).transfer(amount);
+        emit PaymentClaimed(_msgSender(), name, block.timestamp);
+    }
+
+    function getBalance() external view onlyOwner returns (uint256) {
+        return address(this).balance;
+    }
+
+    function shutdown() public onlyFromFactory {
+        selfdestruct(payable(owner()));
+    }
+}
