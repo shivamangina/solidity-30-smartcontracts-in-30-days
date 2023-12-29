d92e971f72075c7eabf0c7968550219f93f5fba0
Shiva Kumar Mangina <50368708+shivamangina@users.noreply.github.com>
2023-12-29 11:15:48 +0530

    Update Day04-Voting.sol

--- Day04-Voting.sol
+++ Day04-Voting.sol
@@ -1,110 +1,63 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.13;
-
-// EVENTS
-// FUNCTION MODIFIERS
-// for loop
+pragma solidity 0.8.19;
 
 contract Voting {
-
-    bool public votingInProgress = false;
-
     struct Voter {
         bool isEligible;
-        bool voted;  // if true, that person already voted
-        uint voteTo;   // index of the voted proposal
+        bool voted;
+        uint256 votedTo; // index of the proposal
     }
-
-    // This is a type for a single proposal.
     struct Proposal {
-        bytes32 name;   // short name (up to 32 bytes)
-        uint voteCount; // number of accumulated votes
+        bytes32 name;
+        uint256 voteCount;
     }
 
     address public chairperson;
-
-    // This declares a state variable that
-    // stores a `Voter` struct for each possible address.
+    bool public votingInProgress = false;
     mapping(address => Voter) public voters;
-
-    // A dynamically-sized array of `Proposal` structs.
     Proposal[] public proposals;
+    bytes32 winningProposal;
 
-    /// Create a new ballot to choose one of `proposalNames`.
     constructor(bytes32[] memory proposalNames) {
         chairperson = msg.sender;
 
-
-        voters[chairperson].isEligible = true;
-        voters[chairperson].voted = false;
-
-        // For each of the provided proposal names,
-        // create a new proposal object and add it
-        // to the end of the array.
-        for (uint i = 0; i < proposalNames.length; i++) {
-            
-            proposals.push(Proposal({
-                name: proposalNames[i],
-                voteCount: 0
-            }));
+        for (uint256 i = 0; i < proposalNames.length; i++) {
+            proposals.push(Proposal({name: proposalNames[i], voteCount: 0}));
         }
     }
 
-
-     // Give `voter` the right to vote on this ballot.
-    // May only be called by `chairperson`.
-    function giveRightToVote(address voter) external {
-        
-        require(
-            msg.sender == chairperson,
-            "Only chairperson can give right to vote."
-        );
-        require(
-            !voters[voter].voted,
-            "The voter already voted."
-        );
-        voters[voter].voted = false;
+    modifier onlyChairperson() {
+        require(msg.sender == chairperson);
+        _;
     }
 
-    function startElection() public  {
-        // only chairman
+    function startElection() public onlyChairperson {
         votingInProgress = true;
-
     }
 
-    function vote(uint proposal) external {
-        Voter storage sender = voters[msg.sender];
-        require(!sender.isEligible, "Has no right to vote");
-        require(!sender.voted, "Already voted.");
-        sender.voted = true;
-        sender.voteTo = proposal;
-
-        // Increase the voteCount
-        proposals[proposal].voteCount += 1;
-    }
-
-    ///  Computes the winning proposal taking all
-    /// previous votes into account.
-    function winningProposal() public view
-            returns (uint winningProposal_)
-    {
-        uint winningVoteCount = 0;
-        for (uint p = 0; p < proposals.length; p++) {
-            if (proposals[p].voteCount > winningVoteCount) {
-                winningVoteCount = proposals[p].voteCount;
-                winningProposal_ = p;
+    function stopElection() public onlyChairperson {
+        votingInProgress = false;
+        uint256 votingCount = 0;
+        for (uint256 i = 0; i < proposals.length; i++) {
+            if (votingCount < proposals[i].voteCount) {
+                votingCount = proposals[i].voteCount;
+                winningProposal = proposals[i].name;
             }
         }
     }
 
-    // Calls winningProposal() function to get the index
-    // of the winner contained in the proposals array and then
-    // returns the name of the winner
-    function endElection() external view
-            returns (bytes32 winnerName_)
-    {
-        winnerName_ = proposals[winningProposal()].name;
+    function vote(uint256 proposal) public {
+        Voter storage voter = voters[msg.sender];
+        require(voter.voted, "Already Voted");
+        require(!voter.isEligible, "Not Eligible");
+
+        voter.voted = true;
+        voter.votedTo = proposal;
+        proposals[proposal].voteCount++;
     }
 
-
+    function giveRightToVote(address voter) public onlyChairperson {
+        voters[voter].isEligible = true;
+        voters[voter].voted = false;
+    }
 }
