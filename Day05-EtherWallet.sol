36178f8d41210aa066eafe1ec97c47e8a6e6e685
Shiva Kumar Mangina <50368708+shivamangina@users.noreply.github.com>
2023-12-30 11:54:47 +0530

    Update Day05-EtherWallet.sol

--- Day05-EtherWallet.sol
+++ Day05-EtherWallet.sol
@@ -1,29 +1,24 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.13;
-
-/**
-Ether Wallet -
-any one can send ether to this contract
-and the ether will be stored in this contract
-only the owner of this contract can withdraw the ether
-the owner of this contract is the address that deployed this contract
- */
+pragma solidity 0.8.19;
 
 contract EtherWallet {
-    address payable public owner;
+    address public owner;
 
     constructor() {
-        owner = payable(msg.sender);
+        owner = msg.sender;
     }
 
-    function withdraw(uint256 _amount) external {
-        require(msg.sender == owner, "only the owner can call this function");
-        owner.transfer(_amount);
+
+    function withdraw(uint256 amount) external {
+        require(owner==msg.sender,"Only owner can with draw funds");
+        payable(owner).transfer(amount);
+
     }
+
 
     function getBalance() external view returns (uint256) {
         return address(this).balance;
     }
 
-    receive() external payable {}
+    receive() external payable { }
 }
