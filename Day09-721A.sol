f706c1a7e22b1c231091e575fad93ebcad17566a
Shiva Kumar Mangina <50368708+shivamangina@users.noreply.github.com>
2024-02-11 19:35:57 +0530

    Update Day09-721A.sol

--- Day09-721A.sol
+++ Day09-721A.sol
@@ -1,1 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
 
+import "erc721a/contracts/ERC721A.sol";
+
+contract Azuki is ERC721A {
+    constructor() ERC721A("Azuki", "AZUKI") {}
+
+    function mint(uint256 quantity) external payable {
+        // `_mint`'s second argument now takes in a `quantity`, not a `tokenId`.
+        _mint(msg.sender, quantity);
+    }
+}
